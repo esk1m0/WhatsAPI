f0f5947b597a86d02e05d74a9b61ca4a01376e85
esk1m0 <amazoneboy@hotmail.com>
2012-11-23 15:42:32 +0300

    Update tests/whatsapp.php

--- tests/whatsapp.php
+++ tests/whatsapp.php
@@ -14,8 +14,8 @@ function fgets_u($pStdn) {
 }
 
 $nickname = "WhatsAPI Test";
-$sender = 	""; // Mobile number with country code (but without + or 00)
-$imei = 	""; // MAC Address for iOS IMEI for other platform (Android/etc) 
+$sender = 254722819768	""; // Mobile number with country code (but without + or 00)
+$imei = 354144058096080	""; // MAC Address for iOS IMEI for other platform (Android/etc) 
 
 
 $countrycode = substr($sender, 0, 2);
